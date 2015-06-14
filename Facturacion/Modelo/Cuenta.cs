4b9b0b76eaa42673cf2c36a313432c0404696972
darkangel100 <darkjoeg@gmail.com>
2015-06-13 23:40:14 -0500

    modelo cuenta y persona

---
+++ Facturacion/Modelo/Cuenta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Facturacion.Modelo
+{
+    class Cuenta: Persona
+    {
+        private string cla_cuent;
+        private string tip_cuent;
+
+        public string clacuent
+        {
+            get { return cla_cuent; }
+            set { cla_cuent = value; }
+        }
+        public string tipcue
+        {
+            get { return tip_cuent; }
+            set { tip_cuent = value; }
+        }
+    }
+}
---
+++ Facturacion/Modelo/Persona.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Facturacion.Modelo
+{
+    class Persona
+    {
+        private string ced_per;
+        private string ape_per;
+        private string nom_per;
+        private string dir_per;
+        private string tel_per;
+        private string est_per;
+        private List<Persona> listaPersonas = new List<Persona>();
+        private string nombre;
+
+        private Cuenta cuenta;
+
+        public string cedper
+        {
+            get { return ced_per; }
+            set { ced_per = value; }
+        }
+        public string apeper
+        {
+            get { return ape_per; }
+            set { ape_per = value; }
+        }
+        public string nomper
+        {
+            get { return nom_per; }
+            set { nom_per = value; }
+        }
+        public string dirper
+        {
+            get { return dir_per; }
+            set { dir_per = value; }
+        }
+        public string telper
+        {
+            get { return tel_per; }
+            set { tel_per = value; }
+        }
+        public string estper
+        {
+            get { return est_per; }
+            set { est_per = value; }
+        }
+        public List<Persona> ListaPersonas
+        {
+            get { return listaPersonas; }
+            set { listaPersonas = value; }
+        }
+        public Cuenta Cuenta
+        {
+            get { return cuenta; }
+            set { cuenta = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+    }
+}
