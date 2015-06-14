44cd3b4ddbf16d9b635c8749e567151976be9e3d
darkangel100 <darkjoeg@gmail.com>
2015-06-13 23:43:01 -0500

    controlador clientedb

---
+++ Facturacion/Controlador/ClienteDB.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using Facturacion.Modelo;
+using System.Data;
+
+namespace Facturacion.Controlador
+{
+    class ClienteDB
+    {
+        conexion con = new conexion();
+        Persona per = null;
+
+        public Persona getPersona()
+        {
+            if (this.per == null)
+            {
+                this.per = new Persona();
+            }
+            return this.per;
+        }
+        public void setPersona(Persona pers)
+        {
+            this.per = pers;
+        }
+        public int InsertaCliente(Persona per)
+        {
+            MySqlCommand cmd;
+            MySqlConnection cn = con.GetConnection();
+            int resp;
+            try
+            {
+                string sqlcad = "Insert persona Values ('" + per.cedper + "','" + per.apeper + "','" + per.nomper + "','" + per.dirper + "','" + per.telper + "','" + per.estper + "')";
+                cmd = new MySqlCommand(sqlcad, cn);
+                cmd.CommandType = CommandType.Text;
+                cn.Open();
+                resp = cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                resp = 0;
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                resp = 0;
+                throw ex;
+            }
+            cn.Close();
+            cmd = null;
+            per = null;
+            return resp;
+        }
+        public List<Persona> TraeClientes(string est)
+        {
+            ClienteDB per = null;
+            List<Persona> ListaCli = new List<Persona>();
+            MySqlCommand cmd;
+            MySqlConnection cn = con.GetConnection();
+            try
+            {
+                string sqlcad = "Select * from persona where est_per='" + est + "' order by ape_per";
+                cmd = new MySqlCommand(sqlcad, cn);
+                cmd.CommandType = CommandType.Text;
+                cn.Open();
+                MySqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    per = new ClienteDB();
+                    per.getPersona().cedper = dr[0].ToString();
+                    per.getPersona().apeper = dr[1].ToString();
+                    per.getPersona().nomper = dr[2].ToString();
+                    per.getPersona().dirper = dr[3].ToString();
+                    per.getPersona().telper = dr[4].ToString();
+                    per.getPersona().estper = dr[5].ToString();
+
+                    per.getPersona().Nombre = per.getPersona().apeper + " " + per.getPersona().nomper;
+                    ListaCli.Add(per.getPersona());
+                }
+                dr.Close();
+            }
+            catch (MySqlException ex)
+            {
+                per = null;
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                per = null;
+                throw ex;
+            }
+            cn.Close();
+            cmd = null;
+            return ListaCli;
+        }
+        public Persona TraeCliente(string ced)
+        {
+            ClienteDB per = null;
+            MySqlCommand cmd;
+            MySqlConnection cn = con.GetConnection();
+            try
+            {
+                string sqlcad = "Select * from persona Where ced_per='" + ced + "'";
+                cmd = new MySqlCommand(sqlcad, cn);
+                cmd.CommandType = CommandType.Text;
+                cn.Open();
+                MySqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    per = new ClienteDB();
+                    per.getPersona().cedper = dr[0].ToString();
+                    per.getPersona().apeper = dr[1].ToString();
+                    per.getPersona().nomper = dr[2].ToString();
+                    per.getPersona().dirper = dr[3].ToString();
+                    per.getPersona().telper = dr[4].ToString();
+                    per.getPersona().estper = dr[5].ToString();
+                }
+                dr.Close();
+            }
+            catch (MySqlException ex)
+            {
+                per = null;
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                per = null;
+                throw ex;
+            }
+            cn.Close();
+            cmd = null;
+            return per.getPersona();
+        }
+    }
+}
